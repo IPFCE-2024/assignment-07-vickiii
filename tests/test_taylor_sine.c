cd1eb46ec1e7526f8a9a704c2f7549b93bdfcd22
Victoria Vu <au792000@uni.au.dk>
2025-10-18 17:44:37 +0200

    done

--- tests/test_taylor_sine.c
+++ tests/test_taylor_sine.c
@@ -25,3 +25,15 @@ int main() {
   return 0;
 
 }
+
+//
+//Funktionen taylor_sine(x,n) blev testet. 
+//Der blev testet med en fast x-værdi, hvor x=3,14, og en variende n-værdi. 
+//Resultaterne blev sammenlignet med  den indbyggede sin⁡(x) funktion fra ANSI C math.h biblioteket.
+//Testene viser, at Taylor-approksimationen er følsom over for antallet af n. 
+//Ved en lav værdi af n (f.eks. n=4) så afviger resultatet markant fra den korrekte værdi, 
+//som kan betyde, at Taylor-rækken ikke har konvergeret tilstrækkeligt endnu. 
+//Allerede ved n=7 er der kommet en forbedring, da de to resultater kom tættere på hinanden end n=4 resultaterne. 
+//Fra n=10 og opefter, så bliver Taylor-resultaterne identiske. 
+//Så min taylor_sine() funktion afhænger af værdien af n, og den virker først når n=10 eller opefter. 
+//
